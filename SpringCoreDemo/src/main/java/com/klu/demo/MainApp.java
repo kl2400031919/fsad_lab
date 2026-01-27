af7adf0dbc8f007f068b35d01058a7ccd0796d55
kl2400031919 <2400031919@kluniversity.in>
2026-01-27 08:46:33 +0530

    commit(27-01-2026)
    commit(27-01-2026)

---
+++ SpringCoreDemo/src/main/java/com/klu/demo/MainApp.java
@@ -0,0 +1,18 @@
+package com.klu.demo;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		org.springframework.context.ApplicationContext  context=new ClassPathXmlApplicationContext("applicationContext.xml");
+		Car car=(Car)context.getBean("car");
+		car.drive();
+		
+		Bike bike=(Bike)context.getBean("bike");
+		bike.ride();
+
+	}
+
+}
